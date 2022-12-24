266120ea479ffa1bdafcbd815ce1d93002c5a2ba
Iara Pou <92182454+Iara-Pou@users.noreply.github.com>
2022-12-24 19:25:25 -0300

    Crea función que distinga si es o no vocal
    La idea es que si es vocal, concateno el término al textoEncriptado, si es consonante, solo sumo el carácter  al textoEncriptado.

--- script.js
+++ script.js
@@ -4,4 +4,17 @@ const $botonDesencriptar = document.querySelector("#boton-desencriptar");
 $botonEncriptar.onclick = encriptar;
 
 function encriptar (){
+const textoIngresado = document.querySelector("#textarea-ingreso").value;
+let textoEncriptado = "";
+
+for(let i = 0; i<textoIngresado.length; i++){
+    //distinguir si es vocal o no
+    const letra = textoIngresado[i];
+    const esVocal = avisarSiEsVocal(letra)
+}
+
+}
+
+function avisarSiEsVocal (letraComparada){
+    return letraComparada === "a" || letraComparada==="e"|| letraComparada==="i"|| letraComparada==="o"|| letraComparada==="u";
 }
