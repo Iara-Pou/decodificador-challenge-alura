7997da608e1b94eaf12b394bd61640f588d4dd66
Iara Pou <92182454+Iara-Pou@users.noreply.github.com>
2022-12-24 19:11:35 -0300

    Asigna la función encriptar al boton encriptar

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
 const $botonEncriptar = document.querySelector("#boton-encriptar");
 const $botonDesencriptar = document.querySelector("#boton-desencriptar");
+
+$botonEncriptar.onclick = encriptar;
+
+function encriptar (){
+}
