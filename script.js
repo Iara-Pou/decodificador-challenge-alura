f5f2ce011871a49faf0c6cc280cc9c83df61d479
Iara Pou <92182454+Iara-Pou@users.noreply.github.com>
2022-12-25 17:39:33 -0300

    Crea un objeto con codificación
    Tiene dos objetos dentro, uno útil para el pasaje de vocal a código (la encriptacion) y otro para el proceso reverso (desencriptación).

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 const $botonEncriptar = document.querySelector("#boton-encriptar");
 const $botonDesencriptar = document.querySelector("#boton-desencriptar");
+const CODIFICACION = {
+    VOCAL_A_CODIGO : {
+        "a": "ai",
+        "e": "enter",
+        "i":"imes",
+        "o":"ober",
+        "u": "ufat"
+    },
+    CODIGO_A_VOCAL : {
+        "ai":"a",
+        "enter":"e",
+        "imes":"i",
+        "ober":"o",
+        "ufat": "u"
+    }
+}
 
 $botonEncriptar.onclick = encriptar;
 
