02b93801bf3a080998c91145cca33286f4b176de
Iara <iarapou21@hotmail.com>
2022-12-30 21:48:06 -0300

    Ordena código
    Primero pone las funciones, seguidas de las variables declaradas y la asignación de funciones.

--- script.js
+++ script.js
@@ -1,18 +1,3 @@
-const $botonEncriptar = document.querySelector("#boton-encriptar");
-const $botonDesencriptar = document.querySelector("#boton-desencriptar");
-const $botonCopiar = document.querySelector("#boton-copiar");
-const VOCAL_A_CODIGO = {
-    "a": "ai",
-    "e": "enter",
-    "i": "imes",
-    "o": "ober",
-    "u": "ufat"
-}
-
-$botonEncriptar.onclick = manejarEncriptado;
-$botonDesencriptar.onclick = manejarDesencriptado;
-$botonCopiar.onclick = copiarTexto;
-
 function copiarTexto(){
     let texto = document.querySelector('#texto-final').textContent;
     navigator.clipboard.writeText(texto);
@@ -87,7 +72,7 @@ function encriptarTexto(textoIngresado) {
 
     return textoEncriptado;
 
-    }
+}
 
 function avisarSiEsVocal(letra) {
     return letra === "a" || letra === "e" || letra === "i" || letra === "o" || letra === "u";
@@ -145,5 +130,17 @@ function mostrarResultado(traduccion) {
     textoFinal.textContent = traduccion;
 }
 
+const $botonEncriptar = document.querySelector("#boton-encriptar");
+const $botonDesencriptar = document.querySelector("#boton-desencriptar");
+const $botonCopiar = document.querySelector("#boton-copiar");
+const VOCAL_A_CODIGO = {
+    "a": "ai",
+    "e": "enter",
+    "i": "imes",
+    "o": "ober",
+    "u": "ufat"
+}
 
-
+$botonEncriptar.onclick = manejarEncriptado;
+$botonDesencriptar.onclick = manejarDesencriptado;
+$botonCopiar.onclick = copiarTexto;
